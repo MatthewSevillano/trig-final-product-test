3d33d515ed21b1bb094387df51bc63781d411a03
MatthewSevillano <matthew.sevillano@ocsbstudent.ca>
2017-05-29 13:18:36 -0400

    work

--- src/trigget.java
+++ src/trigget.java
@@ -19,6 +19,7 @@ public class trigget {
 			double C = in.nextDouble();
 			System.out.println("enter side c length");
 			double c=in.nextDouble();
+		//2 sides 1 angle one pair
 			if (A !=0 && a!= 0&& b!=0){
 				 B=trig.sinLawAngle(A, a, b);
 				System.out.format("angle b is %,.02f degress",B);
@@ -26,15 +27,29 @@ public class trigget {
 				 System.out.format("\nangle c is %,.02f degress",C);
 				 c=trig.sinLawSide(A, a, C);
 				 System.out.format("\nside c is %,.02f",c);
-			
-			} if (a!=0 && b!=0 && C!=0){
+			//2 sides 1 angle no pairs
+			}else if (a!=0 && b!=0 && C!=0){
 				c=trig.cosLawSide(C, b, a);
 				System.out.format("c is %,.02f",c);
 				A=trig.sinLawAngle(C,b,c);
 				System.out.format("\nA is %,.02f degress",A);
-			} if (B!=0 && b!=0 && c!=0){
-				c=trig.cosLawSide(B, b, c);
-				System.out.format("c is %,.02f",c);
+				B=180-(C+A);
+				 System.out.format("\nangle B is %,.02f degress",B);
+			//3 sides
+			} else if (a!=0 && b!=0 && c!=0){
+				C=trig.cosLawAngle(c, b, a);
+				System.out.format("\nangle C is %,.02f degress",C);
+				A=trig.sinLawAngle(C,b,c);
+				System.out.format("\nA is %,.02f degress",A);
+				B=180-(C+A);
+				 System.out.format("\nangle B is %,.02f degress",B);
+			//1 side 2 angles
+			}else if (A!=0 && B!=0 && c!=0){
+				C=180-(B+A);
+				System.out.format("\nangle c is %,.02f degress",C);
+				a=trig.sinLawSide(A, b, C);
+				System.out.format("\nside a is %,.02f degress",a);
+				
 			}
-		}
-	}
+}
+}
